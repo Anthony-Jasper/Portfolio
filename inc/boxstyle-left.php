aab2ddfcceb2c4b18ef6817c056efeccd7115ce7
Anthony Jasper <anthonyjasper112@gmail.com>
2020-05-12 13:40:07 +0100

    Html Layout for Header

---
+++ inc/boxstyle-left.php
@@ -0,0 +1,16 @@
+<div class="boxstyle-container">
+        <div class="boxstyle-left">
+          <div class="boxstyle-diamond"></div> <!-- Diamond -->
+          <div class="boxstyle-linedown"> <!-- Down -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+            <div class="boxstyle-linelong"></div> <!-- Line -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+          </div>
+          <p><span>Developer</span>View Websites and Coding Projects</p>
+          <div class="boxstyle-lineright"> <!-- Right-->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+            <div class="boxstyle-linelong"></div> <!-- Line -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+          </div>
+        </div>
+      </div>
---
+++ inc/boxstyle-right.php
@@ -0,0 +1,16 @@
+<div class="boxstyle-container">
+        <div class="boxstyle-right">
+          <div class="boxstyle-diamond"></div> <!-- Diamond -->
+          <div class="boxstyle-lineup"> <!-- Up -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+            <div class="boxstyle-linelong"></div> <!-- Line -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+          </div>
+          <p><span>Designer</span>View Design Projects</p>
+          <div class="boxstyle-lineleft"> <!-- Left -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+            <div class="boxstyle-linelong"></div> <!-- Line -->
+            <div class="boxstyle-lineshort"></div> <!-- ShortLine -->
+          </div>
+        </div>
+      </div>
